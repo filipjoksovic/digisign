cdfdc50c062c05f952914b8ad781945d0bd9fff9
Filip Joksovic <filipjoksovic1@gmail.com>
2023-03-27 23:15:22 +0200

    feat: Initial GUI signing implementation

--- src/main/java/digisign/signers/DigitalSigner.java
+++ src/main/java/digisign/signers/DigitalSigner.java
@@ -1,15 +1,23 @@
 package digisign.signers;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.security.*;
+import java.security.interfaces.DSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class DigitalSigner {
 
     public static void createKeys(String location) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, IOException, SignatureException {
         System.out.println(location);
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA","SUN");
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG","SUN");
-        keyPairGenerator.initialize(1024,random);
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA", "SUN");
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyPairGenerator.initialize(1024, random);
         KeyPair pair = keyPairGenerator.generateKeyPair();
         PrivateKey privateKey = pair.getPrivate();
         PublicKey publicKey = pair.getPublic();
@@ -21,5 +29,63 @@ public class DigitalSigner {
         publicKeyFos.close();
         privateKeyFos.write(privateKeyEncoded);
         privateKeyFos.close();
+    }
+
+    public static PublicKey readPublicKey(File file) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] key = new byte[fis.available()];
+        fis.read(key);
+        KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(key);
+        return (PublicKey) keyFactory.generatePublic(keySpec);
+    }
+
+    public static PrivateKey readPrivateKey(File file) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException {
+        FileInputStream fis = new FileInputStream(file);
+        byte[] key = new byte[fis.available()];
+        fis.read(key);
+        KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(key);
+        return (PrivateKey) keyFactory.generatePrivate(keySpec);
+    }
+
+    public static void signFile(File file, PrivateKey privateKey, PublicKey publicKey, String signedPath) throws NoSuchAlgorithmException, NoSuchProviderException, IOException, SignatureException, InvalidKeyException {
+        Signature dsaInstance = Signature.getInstance("SHA1withDSA", "SUN");
+        dsaInstance.initSign(privateKey);
+        FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+        BufferedInputStream bufin = new BufferedInputStream(fis);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = bufin.read(buffer)) >= 0) {
+            dsaInstance.update(buffer, 0, len);
+        }
+        ;
+        bufin.close();
+        byte[] realSig = dsaInstance.sign();
+        FileOutputStream sigfos = new FileOutputStream(signedPath); //saving signature in the file sig
+        sigfos.write(realSig); //writes in the file
+        sigfos.close(); //closes the file
+    }
+
+    public static void verifySignature(PublicKey pubKey, File signedFile, File originalFile) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        FileInputStream sigfis = new FileInputStream(signedFile); //specify the file name that contains signature
+        byte[] sigToVerify = new byte[sigfis.available()];  //contains the signature bytes
+        sigfis.read(sigToVerify);
+        sigfis.close();
+        Signature sig = Signature.getInstance("SHA1withDSA", "SUN"); //object of Signature class provides algorithm name with the provider
+        sig.initVerify(pubKey);
+        FileInputStream datafis = new FileInputStream(originalFile);  //specify the file name for which digital signature was generated
+        BufferedInputStream bufin2 = new BufferedInputStream(datafis);
+        byte[] buffer2 = new byte[1024];
+        int len2;
+        while (bufin2.available() != 0) {
+            len2 = bufin2.read(buffer2);
+            sig.update(buffer2, 0, len2);
+        }
+        bufin2.close();
+        boolean verifies = sig.verify(sigToVerify); //returns true if the signature matches
+        System.out.println("signature verifies: " + verifies);
+
+
     }
 }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class App {
     JFrame mainFrame;
@@ -18,20 +20,45 @@ public class App {
     JButton createKeyButton;
     JLabel createKeyLabel;
 
+    JButton uploadFileButton;
+    JButton signFileButton;
+    JButton privateKeyPickerButton;
+    JLabel privateKeyLocationLabel;
+    JButton publicKeyPickerButton;
+    JLabel publicKeyLocationLabel;
 
-    public void init(){
-       this.mainFrame = new JFrame();
+    JButton signDocumentButton;
+    JButton verifySignatureButton;
 
-        this.initKeyGenSection();
+    File fileToSign;
+    File privateKeyFile;
+    File publicKeyFile;
+
+    PublicKey publicKey;
+    PrivateKey privateKey;
+
+
+    public void init() {
+        this.mainFrame = new JFrame();
+
 
         mainFrame.setTitle("Digisign");
-        mainFrame.setSize(400,800);
+        mainFrame.setSize(400, 800);
         mainFrame.setLayout(null);
         mainFrame.setVisible(true);
+
+        this.initKeyGenSection();
+        this.initFileUploadSection();
+        this.initPublicKeyPickerSection();
+        this.initPrivateKeyPickerSection();
+        this.initDocumentSignSection();
+        this.initDocumentVerifySection();
+
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
 
-    public void initKeyGenSection(){
+    public void initKeyGenSection() {
         createKeyButton = new JButton("Create key");
         createKeyButton.setBounds(this.padding, this.padding, this.width - 2 * padding, 30);
 
@@ -53,8 +80,7 @@ public class App {
                                 "Success",
                                 JOptionPane.INFORMATION_MESSAGE);
                     }
-                }
-                catch(Exception ex){
+                } catch (Exception ex) {
 
                     System.out.println("Error occured");
                 }
@@ -63,7 +89,187 @@ public class App {
         mainFrame.add(createKeyButton);
     }
 
-    public static void main(String[] args){
+    public void initFileUploadSection() {
+        System.out.println("Adding");
+        uploadFileButton = new JButton("Pick a file");
+        uploadFileButton.setBounds(padding, createKeyButton.getY() + padding + 35, mainFrame.getWidth() - 2 * padding, 30);
+
+
+        uploadFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked file upload button");
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = fileChooser.showOpenDialog(mainFrame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    fileToSign = fileChooser.getSelectedFile();
+                    System.out.println("Selected file: " + fileToSign.getAbsolutePath());
+                }
+            }
+        });
+        mainFrame.add(uploadFileButton);
+        mainFrame.repaint();
+    }
+
+    public void initPublicKeyPickerSection() {
+        System.out.println("Adding");
+        publicKeyPickerButton = new JButton("Pick a public key");
+        publicKeyPickerButton.setBounds(padding, uploadFileButton.getY() + padding + 35, mainFrame.getWidth() / 2 - 2 * padding, 30);
+
+
+        publicKeyPickerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked file upload button");
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Select location for the public key");
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = fileChooser.showOpenDialog(mainFrame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    publicKeyFile = fileChooser.getSelectedFile();
+                    publicKeyLocationLabel = new JLabel("Key location: " + publicKeyFile.getAbsolutePath());
+                    publicKeyLocationLabel.setBounds(padding, publicKeyPickerButton.getY() + padding, mainFrame.getWidth() / 2 - 2 * padding, 50);
+                    mainFrame.add(publicKeyPickerButton);
+                    mainFrame.repaint();
+
+                    System.out.println("Selected file: " + publicKeyFile.getAbsolutePath());
+                    try {
+                        publicKey = DigitalSigner.readPublicKey(publicKeyFile);
+                        JOptionPane.showMessageDialog(mainFrame,
+                                "Public Key successfully parsed" ,
+                                "Success",
+                                JOptionPane.INFORMATION_MESSAGE);
+
+                    }
+                    catch (Exception exception){
+                        System.out.println("Error while parsing public key");
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        });
+        mainFrame.add(publicKeyPickerButton);
+        mainFrame.repaint();
+    }
+
+    public void initPrivateKeyPickerSection() {
+        System.out.println("Adding");
+        privateKeyPickerButton = new JButton("Pick a private key");
+        privateKeyPickerButton.setBounds(padding + publicKeyPickerButton.getWidth() + padding, publicKeyPickerButton.getY(), mainFrame.getWidth() / 2 - 2 * padding, 30);
+
+
+        privateKeyPickerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Clicked file upload button");
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Select location for the private key");
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = fileChooser.showOpenDialog(mainFrame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    privateKeyFile = fileChooser.getSelectedFile();
+                    privateKeyLocationLabel = new JLabel("Private key location: " + privateKeyFile.getAbsolutePath());
+                    privateKeyLocationLabel.setBounds(padding, privateKeyPickerButton.getY() + padding, mainFrame.getWidth() / 2 - 2 * padding, 50);
+                    mainFrame.add(privateKeyPickerButton);
+                    mainFrame.repaint();
+
+                    System.out.println("Selected file: " + privateKeyFile.getAbsolutePath());
+                    try {
+                        privateKey = DigitalSigner.readPrivateKey(privateKeyFile);
+                        JOptionPane.showMessageDialog(mainFrame,
+                                "Private key successfully parsed" ,
+                                "Success",
+                                JOptionPane.INFORMATION_MESSAGE);
+
+                    }
+                    catch (Exception exception){
+                        System.out.println("Error while parsing public key");
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        });
+        mainFrame.add(privateKeyPickerButton);
+        mainFrame.repaint();
+    }
+
+    public void initDocumentSignSection() {
+        System.out.println("Adding");
+        signDocumentButton = new JButton("Sign document");
+        signDocumentButton.setBounds(padding, publicKeyPickerButton.getY() + padding * 2, mainFrame.getWidth() - 2 * padding, 30);
+        signDocumentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JFileChooser fileChooser = new JFileChooser();
+                    fileChooser.setDialogTitle("Select a location for storing signed file");
+                    int selection = fileChooser.showSaveDialog(mainFrame);
+                    if (selection == JFileChooser.APPROVE_OPTION) {
+                        File signedDocument = fileChooser.getSelectedFile();
+                        DigitalSigner.signFile(fileToSign,privateKey,publicKey, signedDocument.getPath());
+                        System.out.println("Save as file: " + signedDocument.getAbsolutePath());
+                        JOptionPane.showMessageDialog(mainFrame,
+                                "Document signed and stored at: " + signedDocument.getAbsolutePath(),
+                                "Success",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } catch (Exception ex) {
+
+                    System.out.println("Error occured");
+                }
+            }
+        });
+        mainFrame.add(signDocumentButton);
+        mainFrame.repaint();
+    }
+
+    public void initDocumentVerifySection() {
+        System.out.println("Adding");
+        verifySignatureButton = new JButton("Verify signature");
+        verifySignatureButton.setBounds(padding, signDocumentButton.getY() + padding * 2, mainFrame.getWidth() - 2 * padding, 30);
+        verifySignatureButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JFileChooser signedFileChooser = new JFileChooser();
+                    signedFileChooser.setDialogTitle("Select a signed file");
+                    int selection = signedFileChooser.showOpenDialog(mainFrame);
+                    File signedFile = null;
+                    if (selection == JFileChooser.APPROVE_OPTION) {
+                        signedFile = signedFileChooser.getSelectedFile();
+                    }
+
+                    signedFileChooser.setDialogTitle("Select original file");
+                    selection = signedFileChooser.showOpenDialog(mainFrame);
+                    File originalFile = null;
+                    if(selection == JFileChooser.APPROVE_OPTION){
+                        originalFile = signedFileChooser.getSelectedFile();
+                    }
+
+                    signedFileChooser.setDialogTitle("Select public key for verification");
+                    selection = signedFileChooser.showOpenDialog(mainFrame);
+                    File publicKeyFile = null;
+                    if(selection == JFileChooser.APPROVE_OPTION){
+                        publicKeyFile = signedFileChooser.getSelectedFile();
+                    }
+
+                    PublicKey pubKey = DigitalSigner.readPublicKey(publicKeyFile);
+
+                    if(pubKey != null && originalFile != null && signedFile != null){
+                        DigitalSigner.verifySignature(pubKey,signedFile,originalFile);
+                    }
+
+                } catch (Exception ex) {
+                    System.out.println("Error occured");
+                }
+            }
+        });
+        mainFrame.add(verifySignatureButton);
+        mainFrame.repaint();
+    }
+
+    public static void main(String[] args) {
         App app = new App();
         app.init();
     }
